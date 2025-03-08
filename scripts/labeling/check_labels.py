6820a71d3e1e0093c176de3f3a5ea688d3b993de
mozhongzhou <2014160588@qq.com>
2025-03-08 19:37:02 +0800

    初始化新的分支,该分支不用finBert处理报告,仅用transformer

--- scripts/labeling/check_labels.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-labels = np.load("E:/Coding/NLP-Project/StockPrediction/data/processed/multimodal/NVDA_labels.npy", allow_pickle=True)
-print(labels.shape, labels[:5])
--- scripts/test_env.py
+++
@@ -1,4 +0,0 @@
-import torch
-print(torch.__version__)  # 打印PyTorch版本
-print(torch.cuda.is_available())  # 检查CUDA是否可用
-print(torch.version.cuda)  # 打印PyTorch支持的CUDA版本
